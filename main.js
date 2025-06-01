7ff046ef747e9735091ef5d874b8181d56c50e76
elnakhlawi <mahmoudelnakhlawy1@gmail.com>
2025-06-01 22:34:38 +0300

    add dark mode to btn

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let iDarkMode=document.getElementById('iDarkMode');
+let btnSubmit=document.getElementById('submit');
+
 iDarkMode.addEventListener('click',() => { 
   document.body.classList.toggle('body-dark-mode');
-  iDarkMode.classList.toggle('i-dark-mode-active')
+  iDarkMode.classList.toggle('i-dark-mode-active');
+  btnSubmit.classList.toggle('btn-darke-mode');
+
  })
