bec2acc255e58190ab3dc095fabd7bfc29aac6a3
elnakhlawi <mahmoudelnakhlawy1@gmail.com>
2025-06-02 09:56:23 +0300

    outputs section done

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let iDarkMode=document.getElementById('iDarkMode');
 let btnSubmit=document.getElementById('submit');
-
+let searchByTitle=document.getElementById('searchByTitle');
+let searchByCategory=document.getElementById('searchByCategory');
+console.log(searchByTitle);
 iDarkMode.addEventListener('click',() => { 
   document.body.classList.toggle('body-dark-mode');
   iDarkMode.classList.toggle('i-dark-mode-active');
   btnSubmit.classList.toggle('btn-darke-mode');
+  searchByTitle.classList.toggle('btn-darke-mode');
+  searchByCategory.classList.toggle('btn-darke-mode');
 
  })
