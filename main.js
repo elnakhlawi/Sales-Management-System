45a1d5e3eba69da338e2363ce283061d59300ab8
elnakhlawi <mahmoudelnakhlawy1@gmail.com>
2025-06-02 11:56:24 +0300

    get total function done

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+
+// start dark mode 
 let iDarkMode=document.getElementById('iDarkMode');
 let btnSubmit=document.getElementById('submit');
 let searchByTitle=document.getElementById('searchByTitle');
 let searchByCategory=document.getElementById('searchByCategory');
-console.log(searchByTitle);
+
 iDarkMode.addEventListener('click',() => { 
   document.body.classList.toggle('body-dark-mode');
   iDarkMode.classList.toggle('i-dark-mode-active');
@@ -11,3 +13,71 @@ iDarkMode.addEventListener('click',() => {
   searchByCategory.classList.toggle('btn-darke-mode');
 
  })
+ //end dark mode
+
+ //start get all   buttons and inputs from inputs section
+ let title=document.getElementById('title');
+ let price=document.getElementById('price');
+ let taxes=document.getElementById('taxes');
+ let ads=document.getElementById('ads');
+ let discount=document.getElementById('discount');
+ let total=document.getElementById('total');
+ let count=document.getElementById('count');
+ let category=document.getElementById('category');
+ let submit=document.getElementById('submit');
+ //end get all   buttons and inputs from inputs section
+
+ // start get total
+function getTotal(){
+  if(price.value !=''){
+  let result=(+price.value + +taxes.value + +ads.value) - +discount.value;
+  total.innerHTML=result;
+  total.style.backgroundColor='green';
+  }else{
+    total.innerHTML='';
+    total.style.backgroundColor='rgb(223, 91, 91)';
+  }
+}
+
+
+
+
+//end get total
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //create product
+ //save localstorage
+ //clear inputs
+ //read  
+ //count
+ //delete 
+ //update
+ //search 
+ //clean data
